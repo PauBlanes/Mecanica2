548ad2963e82a1ee4e3b53ac6ace301505c40a52
bzheng <bzheng@S5-23>
2017-03-14 11:40:07 +0100

    add task

--- GL_framework/src/physics.cpp
+++ GL_framework/src/physics.cpp
@@ -1,6 +1,28 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_glfw_gl3.h>
 
+/*
+TASK
+1. Simulation of the whole mesh using your solver of choice (Euler or Verlet). (4pt)
+	- particle-spring mesh of 14 x 18 points
+	- mass equal to 1 kg
+	- the mesh should be animated except for two at the top, which should have a fixed position (points 0 and 13).
+	- mesh such that it is horizontal at a initial height and make the position
+	- runs for 20 seconds, it should reinitialize
+
+2. Collision with walls and ground planes. (2pt)
+
+3. Collision with the Sphere. (2pt)
+	- random location
+	- radius of the sphere random.
+
+4. Make the parameters tweakable from the GUI. (2pt)
+	 Constant and damping term of direct-link springs (stretch).
+	 Constant and damping term of diagonal-link springs (shear).
+	 Constant and damping term of second-link springs (bend).
+	 Max % of accepted ellongation of links.
+	 Initial Rest distance of the springs between the points of the mesh.
+*/
 bool show_test_window = false;
 void GUI() {
 	{	//FrameRate
